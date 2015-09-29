cb9c643663b0fc91730a9328518986f24ee953b8
Christopher Mack <mack.c@ghc.org>
2015-09-29 14:21:33 -0700

    Removed policy scope from events (practices-partners habtm failing)

--- app/policies/event_policy.rb
+++ app/policies/event_policy.rb
@@ -4,7 +4,8 @@ class EventPolicy < ApplicationPolicy
 			if user.admin?
 				scope.all
 			else
-				scope.joins(:partners).where('partners.site_id' => user.site_id)
+				scope.all
+				# scope.joins(:partners).where('partners.site_id' => user.site_id)
 			end
 		end
 	end
