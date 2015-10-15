43a044789736dccf541eeb2999a605837b5acfc5
Roy Pardee <pardee.r@ghc.org>
2015-10-15 12:10:42 -0700

    Moved cache-resetting code to migration.

--- db/migrate/20151014211802_add_cache_fields_to_practice.rb
+++ db/migrate/20151014211802_add_cache_fields_to_practice.rb
@@ -1,14 +1,22 @@
+# https://stackoverflow.com/questions/17226902/custom-helper-methods-for-rails-3-2-migrations
+require File.expand_path('lib/cache_helper')
+include CacheHelper
+
 class AddCacheFieldsToPractice < ActiveRecord::Migration
   # Problem is bad perf (n+1 queries) on /partners/show for partners
   # that have a lot of associated practices.  Querying the associated
   # personnells (for primary contact) and events (for last activity & PAL status)
-  # Takes way too long.  Try adding fields to practice to hold values we will
+  # takes way too long.  Try adding fields to practice to hold values we will
   # manage with activerecord callbacks on personnells and events.
-  def change
+  def up
     add_column :practices, :pc_name_cached, :string
     add_column :practices, :la_date_cached, :date
     add_column :practices, :pal_status_cached, :string
-    execute "update practices set pc_name_cached = 'None Assigned'"
-    execute "update practices set pal_status_cached = false"
+    CacheHelper.reset_cache_money
+  end
+  def down
+    remove_column :practices, :pc_name_cached, :string
+    remove_column :practices, :la_date_cached, :date
+    remove_column :practices, :pal_status_cached, :string
   end
 end
