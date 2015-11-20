26c046e56ba4908a3dae143447347bd20442cd76
Roy Pardee <pardee.r@ghc.org>
2015-11-20 09:05:17 -0800

    Fixed typo in practices edit form; added more fields to export (though still not finished).

--- app/helpers/practices_helper.rb
+++ app/helpers/practices_helper.rb
@@ -20,26 +20,54 @@ module PracticesHelper
   end
 
   def export(practices)
-  to_export = {study_id:              'Study ID',
-                name:                 'Name',
-                address:              'Address',
-                phone:                'Phone',
-                url:                  'Website',
-                created_at:           'Record Created At',
-                updated_at:           'Last DB update',
-                email:                'E-mail',
-                recruitment_source:   'Recruitment Source',
-                prac_ehr:             'EHR',
-                current_partner:      'Current Partner',
-                parent_organization:  'Parent Org',
-                prac_state:           'State',
-                zip_code:             'Zip',
-                city:                 'City',
-                pc_name_cached:       'Primary Contact',
-                la_date_cached:       'Last Activity',
-                pal_status_cached:    'PAL Status',
-                site_id:              'Site',
-                id:                   'Database ID'}
+    to_export = {study_id:                      'Study ID',
+                  name:                         'Name',
+                  parent_organization:          'Parent Org',
+                  current_partner:              'Primary Recruiter',
+                  recruitment_source:           'Recruitment Source',
+                  address:                      'Address',
+                  city:                         'City',
+                  prac_state:                   'State',
+                  zip_code:                     'Zip',
+                  phone:                        'Phone',
+                  url:                          'Website',
+                  email:                        'E-mail',
+                  residency_training_site:      'Residency Training Site?',
+                  number_clinicians:            'No. clinicians',
+                  fte_clinicians:               'Clinician FTE',
+                  interest_yn:                  'Interested?',
+                  primary_care:                 'Primary Care',
+                  prac_own_clinician:           'Clinician owned?',
+                  prac_own_hosp:                'Hosp/healthsys owned?',
+                  prac_own_hmo:                 'HMO Owned?',
+                  prac_own_fqhc:                'FQHC Owned?',
+                  prac_own_nonfed:              'Nonfed Owned?' ,
+                  prac_own_academic:            'Academic Owned?',
+                  prac_own_fed:                 'Federal?',
+                  prac_own_rural:               'Rural?',
+                  prac_own_ihs:                 'IHS?',
+                  prac_own_other:               'Other Owned',
+                  prac_own_yrs:                 'Years Owned',
+                  elig_clinic_count:            'PC Count',
+                  prac_spec_mix:                'Specialty Mix',
+                  prac_pcmh:                    'PCMH?',
+                  prac_aco_medicaid:            'Medicaid ACO',
+                  prac_aco_medicare:            'Medicare ACO' ,
+                  prac_aco_commercial:          'Private/Commercial ACO',
+                  prac_aco_other:               'Other ACO',
+                  prac_aco_none:                'Not an ACO',
+                  prac_aco_join_medicaid:       'Join Medicaid ACO?',
+                  prac_aco_join_medicare:       'Join Medicare ACO?',
+                  prac_aco_join_commercial:     'Join Commercial ACO?',
+                  prac_ehr:                     'EHR',
+                  prac_ehr_extractdata:         'Have data extractor?',
+                  prac_ehr_person_extractdata:  'Data Extractor',
+                  prac_it_support:              'Have HIT support?',
+                  pc_name_cached:               'Primary Contact',
+                  la_date_cached:               'Last Activity',
+                  pal_status_cached:            'PAL Status',
+                  site_id:                      'Site',
+                  id:                           'Database ID'}
 
     csv_string = CSV.generate do |csv|
       csv << to_export.values
@@ -53,10 +81,18 @@ module PracticesHelper
                                     Practice::PRAC_EHRNAME_VALS.key(user.send(k))
                                   when :recruitment_source
                                     Practice::RECRUITMENT_SOURCE_VALS.key(user.send(k))
+                                  when :primary_care, :interest_yn
+                                    Practice::YN12_VALS.key(user.send(k))
+                                  when :prac_spec_mix
+                                    Practice::PRAC_SPEC_MIX_VALS.key(user.send(k))
+                                  when :prac_ehr_extractdata
+                                    Practice::PRAC_EHR_EXTRACTDATA_VALS.key(user.send(k))
+                                  when :prac_ehr_person_extractdata
+                                    Practice::PRAC_EHR_PERSON_EXTRACTDATA_VALS.key(user.send(k))
                                   when :site_id
                                     user.site ? user.site.name : 'Not assigned'
                                   else
-                                      user.send(k)
+                                    user.send(k)
                                   end
 
         end
