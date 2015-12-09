03b30154ecea1e320e8b14b3c1f825a65078497e
Christopher Mack <mack.c@ghc.org>
2015-12-09 15:27:52 -0800

    Small display changes to coach_practices

--- app/helpers/coach_practices_helper.rb
+++ app/helpers/coach_practices_helper.rb
@@ -1,11 +1,11 @@
 module CoachPracticesHelper
 	def get_gyr_html(gyr_value)
 		if Ivcontact::GYR_VALS.key(gyr_value) == "Green"
-			html = "<span style='color: green;'>Green</span>"
+			html = "<span style='background-color: green; color: white; padding: 3px;'>Green</span>"
     elsif Ivcontact::GYR_VALS.key(gyr_value) == "Yellow"
-      html = "<span style='color: yellow;'>Yellow</span>"
+      html = "<span style='background-color: yellow; padding: 3px';>Yellow</span>"
  		elsif Ivcontact::GYR_VALS.key(gyr_value) == "Red"
-      html = "<span style='color: red;'>Red</span>"
+      html = "<span style='background-color: red; color: white; padding: 3px;'>Red</span>"
     end
     return html.html_safe if html
 	end
