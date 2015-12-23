d73d46c501c4264f590cdb89f7ed10ea2c136c36
Roy Pardee <pardee.r@ghc.org>
2015-12-23 11:27:01 -0800

    Decent prototypes of dashboard tables #2 and #3, and a stubbed-out version of #4 which I'll get to next. Added a 'Managers Area' landing page to collect these things. Added a primitive page-title variable in order to make back-button page lists more informative.  Set @page_title in any controller method to use that--otherwise pages will retain the static "Health Hearts Northwest" title.

--- app/models/ivcontact.rb
+++ app/models/ivcontact.rb
@@ -22,6 +22,74 @@ class Ivcontact < ActiveRecord::Base
     disruptions.gsub(/\*$/, '')
   end
 
+  def hlcs_making_progress
+    lst = ""
+    HLCS_SHORT.keys.each do |f|
+      lst += "#{HLCS_SHORT[f]}; " if self.send(f) == PROGRESS_VALS["Making progress"]
+    end
+    if lst  == ""
+      "None"
+    else
+      lst[0..-3]
+    end
+  end
+
+  def topics_discussed
+    lst = ""
+    i = 1
+    DISCUSSION_TOPICS.keys.each do |f|
+      lst += "<abbr title = '#{DISCUSSION_TOPICS[f]}'>#{i}</abbr>; " if self.send(f)
+      i += 1
+    end
+    if lst  == ""
+      "None"
+    else
+      lst[0..-3] # eat the last semicolon/space
+    end
+  end
+
+  HLCS_SHORT = {
+    milestone_evidence_progress:  "Embed evidence",
+    milestone_data_progress:      "Use data",
+    milestone_qi_progress:        "QI process",
+    milestone_atrisk_progress:    "Identify patients",
+    milestone_task_progress:      "Define roles",
+    milestone_selfmgmt_progress:  "Patient self-management",
+    milestone_community_progress: "Community resources"
+  }
+
+  DISCUSSION_TOPICS = {
+    topic_ehr_vendor:           "Work with EHR vendor",
+    topic_3rdparty_vendor:      "Work with 3rd party vendor",
+    topic_custom_query:         "Work on custom queries",
+    topic_validate_data:        "Validate data",
+    topic_meaningful_use:       "Meaningful Use",
+    topic_cqm_report:           "Generate clinical quality measure reports",
+    topic_abcs:                 "Submit ABCS measures to study team",
+    topic_data_error:           "Data errors",
+    topic_coding:               "Coding/IT needs",
+    topic_hit_display:          "Create data displays",
+    topic_reminder:             "Point of care reminders",
+    topic_self_assessment:      "Review/collect Self-Assessment Survey",
+    topic_share:                "Share best practices from another site",
+    topic_planning:             "Discuss PDSA cycles or action planning",
+    topic_workflow:             "Support workflow design",
+    topic_leadership:           "Leadership meeting",
+    topic_review_guideline:     "Review/discuss organizational guidelines",
+    topic_brainstorm:           "Brainstorm ideas",
+    topic_observe_flow:         "Observe team/clinic flow",
+    topic_consensus:            "Facilitate a consensus discussion",
+    topic_connect:              "Connect practice to another practice",
+    topic_resource:             "Supply a tool or resource to support work",
+    topic_discuss_measurement:  "Discuss value-based measurement",
+    topic_roles:                "Identify roles/responsibilities",
+    topic_qi_support:           "Provide QI meeting support",
+    topic_huddle:               "Huddles",
+    topic_review_data:          "Review data",
+    topic_qi_display:           "Data displays/run-charts",
+    topic_other:                "Other"
+  }
+
   HIT_TIER_VALS = {
     "Tier 1" => 1,
     "Tier 2" => 2,
--- config/initializers/active_record_extension_parse_date_writer.rb
+++ config/initializers/active_record_extension_parse_date_writer.rb
@@ -1,17 +1,33 @@
 # initializers/active_record_extension_parse_date_writer.rb
+# https://stackoverflow.com/questions/24499466/how-to-change-the-way-ruby-on-rails-parses-two-digit-year-input
 module ActiveRecordExtensionParseDateWriter
 
 	private
 
 	def write_attribute(attr_name, value)
-		if self.class.column_types.fetch(attr_name.to_s).type == :date
-			d = (Date.strptime(value.to_s, "%m/%d/%y")) rescue nil
-			super(attr_name, d)
-		else
-			super
-		end
-	end
+    if self.class.column_types.fetch(attr_name.to_s).type == :date
+      d = (pdate(value)) rescue nil
+      super(attr_name, d)
+    else
+      super
+    end
+  end
 
+  def pdate(val)
+    ret = nil
+    case val.to_s
+      # when /\d{4}-\d{1,2}-\d{1,2}/
+      # mdy, forward-slashes w/2 digit year
+      when /\d{1,2}\/\d{1,2}\/\d{2}/
+        strfmt = "%m/%d/%y"
+      # mdy, forward-slashes w/4 digit year
+      when /\d{1,2}\/\d{1,2}\/\d{4}/
+        strfmt = "%m/%d/%Y"
+      else
+        ret = val
+    end
+    return ret || Date.strptime(val.to_s, stfmt)
+  end
 end
 
 # include the extension
